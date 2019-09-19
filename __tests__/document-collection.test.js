d8f79ab93b659a19ef7ca4562222a000cc031252
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-19 13:11:53 -0700

    objects saving and test passing

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -6,7 +6,22 @@ jest.mock('../lib/files.js', () => ({
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
+const DocumentCollection = require('../lib/document-collection');
+const dest = 'banana';
+const newDocuments = new DocumentCollection(dest);
 
 describe('Document Collection', () => {
   // TODO
+  it('takes object and writes to new file', () => {
+    const exampleObject = { name: 'Dylan', artist: true };
+    writeFile.mockResolvedValue(exampleObject);
+
+    return newDocuments.save(exampleObject)
+      .then(returnedObj => {
+        const writeCalls = writeFile.mock.calls;
+        expect(writeCalls.length).toBe(1);
+        expect(writeCalls[0][0]).toBe(`${dest}/${returnedObj.id}.json`);
+        expect(writeCalls[0][1]).toBe(JSON.stringify(exampleObject));
+      });
+  });
 });
