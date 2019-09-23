a5d283a599b039f08f2bbb6b1b918e9fd1e45c53
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-23 13:27:44 -0700

    setting up Lab 4

---
+++ __tests__/Schema.test.js
@@ -0,0 +1,45 @@
+/*eslint-disable-next-line*/
+const Schema = require('../lib/Schema');
+
+describe('Schema', () => {
+  it('true', () => {
+    expect(true).toBe(true);
+  });
+
+//Marty's Demo
+  // add a test schema
+  // const schema = new Schema({
+  //   name : {
+  //     type: String,
+  //     required: true
+  //   },
+  //   isFun: {
+  //     type:Boolean,
+  //     required:false
+  //   },
+  //   luckyNumber: {
+  //     type:Number,
+  //     required: false
+  //   }
+  // });
+
+
+
+  // it('validates a correct model', () => {
+  //   const model = {
+  //     name: 'Good',
+  //     isFun: true,
+  //     luckyNumber: 79
+  //   };
+  //   const record = schema.validate(model);
+
+  //   expect(record).not.toBe(model);
+  //   expect(record).toEqual(model);
+  // });
+
+  // it('throws on invalid model', () => {
+
+  // });
+
+  // more test cases...
+});
